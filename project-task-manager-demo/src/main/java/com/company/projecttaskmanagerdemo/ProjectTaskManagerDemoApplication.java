14aed20e3a6c397a05691f5183908de0f4695c9c
surikpro <aydarzak1@yandex.ru>
2022-02-17 12:57:03 +0300

    initial commit

---
+++ project-task-manager-demo/src/main/java/com/company/projecttaskmanagerdemo/ProjectTaskManagerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.company.projecttaskmanagerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTaskManagerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTaskManagerDemoApplication.class, args);
+	}
+
+}
